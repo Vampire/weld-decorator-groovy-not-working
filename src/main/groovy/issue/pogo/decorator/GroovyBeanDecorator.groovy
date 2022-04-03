71bd487859a66854b60492ef3b8024319e72730b
Björn Kautler <Bjoern@Kautler.net>
2022-04-03 04:43:44 +0200

    Work-around for https://issues.redhat.com/browse/WELD-2713

--- src/main/groovy/issue/pogo/decorator/GroovyBeanDecorator.groovy
+++ src/main/groovy/issue/pogo/decorator/GroovyBeanDecorator.groovy
@@ -5,9 +5,11 @@ import issue.pogo.GroovyBean
 import javax.decorator.Decorator
 import javax.decorator.Delegate
 import javax.enterprise.inject.Any
+import javax.enterprise.inject.Typed
 import javax.inject.Inject
 
 @Decorator
+@Typed(GroovyBean)
 abstract class GroovyBeanDecorator implements GroovyBean {
     @Inject
     @Delegate
